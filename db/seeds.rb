964e3817fd6d1422810593c174525b3e690f423c
gedunlap <g.e.dunlap90@gmail.com>
2021-06-12 16:24:28 -0400

    reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "the one" },
+	{ name: "Trinity", description: "the badass" },
+	{ name: "Morpheus", description: "the leader" },
+	{ name: "Agent Smith", description: "the baddie" },
 ])
